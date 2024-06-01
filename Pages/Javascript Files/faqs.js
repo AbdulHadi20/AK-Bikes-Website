acf6cc74386a305eb07d8c6bd649db95f9dec385
notKrunch <mrlumbess@gmail.com>
2024-06-01 20:03:32 +0400

    .

--- Pages/Javascript Files/faqs.js
+++ Pages/Javascript Files/faqs.js
@@ -12,3 +12,21 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+// faqs.js
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    const navBar = document.getElementById("nav-bar");
+
+    hamburgerMenu.addEventListener("click", function() {
+        navBar.classList.toggle("active");
+    });
+
+    const searchIcon = document.getElementById("search-icon");
+    const searchInput = document.querySelector(".input-search");
+
+    searchIcon.addEventListener("click", function() {
+        searchInput.classList.toggle("expanded");
+    });
+});
