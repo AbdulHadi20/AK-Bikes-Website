16e500505b1d864ce608e93d33c49a8f52748ef4
cha4rls <charlsreyes48@gmail.com>
2024-06-02 18:51:42 +0400

    .

--- Pages/Javascript Files/RentalsPage.js
+++ Pages/Javascript Files/RentalsPage.js
@@ -91,3 +91,32 @@ document.addEventListener('DOMContentLoaded', () => {
   // Initialize default view
   switchView(currentView);
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY> 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchToggle = document.getElementById('search-toggle');
+    const searchContainer = document.getElementById('search-container');
+
+    // Toggle search bar visibility on search button click
+    searchToggle.addEventListener('click', function () {
+        searchContainer.style.display = (searchContainer.style.display === 'block') ? 'none' : 'block';
+    });
+});
