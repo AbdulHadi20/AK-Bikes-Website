7077567f7082f3fba55c45b2b672cf5f395734ab
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2024-06-02 14:31:09 +0400

    .

--- Pages/Javascript Files/homepage.js
+++ Pages/Javascript Files/homepage.js
@@ -33,3 +33,32 @@ document.getElementById('search-icon').addEventListener('click', function() {
 const navBar = document.getElementById('nav-bar');
 navBar.classList.toggle('active');
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY> 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchToggle = document.getElementById('search-toggle');
+    const searchContainer = document.getElementById('search-container');
+
+    // Toggle search bar visibility on search button click
+    searchToggle.addEventListener('click', function () {
+        searchContainer.style.display = (searchContainer.style.display === 'block') ? 'none' : 'block';
+    });
+});
