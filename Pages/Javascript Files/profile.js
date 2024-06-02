c12f12eed780757eb2893d278d8fe3164f051d86
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2024-06-02 19:43:03 +0400

    .

--- Pages/Javascript Files/profile.js
+++ Pages/Javascript Files/profile.js
@@ -36,3 +36,32 @@ function logout() {
     // Add any additional logout functionality here
     window.location.reload(); // This will reload the page, simulating a logout
 }
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY> 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchToggle = document.getElementById('search-toggle');
+    const searchContainer = document.getElementById('search-container');
+
+    // Toggle search bar visibility on search button click
+    searchToggle.addEventListener('click', function () {
+        searchContainer.style.display = (searchContainer.style.display === 'block') ? 'none' : 'block';
+    });
+});
