1c7bc1c502de6ed20ec039fce0ec382f4494feb2
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2024-06-02 15:17:40 +0400

    .

--- Pages/Javascript Files/homepage.js
+++ Pages/Javascript Files/homepage.js
@@ -14,26 +14,6 @@ function showSlides() {
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
-document.getElementById('search-icon').addEventListener('click', function() {
-    const searchInput = document.querySelector('.input-search');
-    if (searchInput.classList.contains('hidden')) {
-      searchInput.classList.remove('hidden');
-      setTimeout(() => {
-        searchInput.classList.add('expanded');
-      }, 10); // Short delay to trigger the transition
-    } else {
-      searchInput.classList.remove('expanded');
-      setTimeout(() => {
-        searchInput.classList.add('hidden');
-      }, 500); // Wait for the transition to complete before hiding
-    }
-  });
-
-  document.getElementById('hamburger-menu').addEventListener('click', function() {
-const navBar = document.getElementById('nav-bar');
-navBar.classList.toggle('active');
-});
-
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
